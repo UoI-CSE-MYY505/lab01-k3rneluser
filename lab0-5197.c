3de358fb7cff5b2389026f90d3a3d601bdedac46
k3rneluser <cs05197@uoi.gr>
2024-10-16 23:03:43 +0300

    This was a first

---
+++ lab0-5197.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int agrc,char *argv[]){
+	char name[] = "Ioannis Gardikiotis";
+	int am = 5197;
+	char title[] = "student";
+	printf("Name : %s\nAM: %d\nTitle: %s\n",name,am,title);
+	return 0;
+
+}
